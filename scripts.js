f7668a84e3bd05262c4811cd3934bd8788cab338
IRIS <irisofdefi@gmail.com>
2025-08-12 10:53:03 +0100

    Experimented with JavaScript array methods

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let adjective = "bright";
+let noun = "john";
+let verb = "hot";
+let place = "small hut";
+let adjective2 = "too big";
+let noun2 = "rice";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "tall";
+noun = "mary";
+verb = "cold";
+place = "big house";
+adjective2 = "too small";
+noun2 = "soup";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("Second story: " + secondStory);
